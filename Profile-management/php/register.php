efc8028aea6af090293faaa3243d6e2029717400
Selva-NSK <99686682+Selva-NSK@users.noreply.github.com>
2023-03-10 19:42:59 +0530

    Update register.php

--- Profile-management/php/register.php
+++ Profile-management/php/register.php
@@ -2,6 +2,14 @@
 $server="localhost";
 $user="root";
 $password="root@123";
-$db="miniproject";
+$db="profile_management";
 $con= mysqli_connect("$server","$user","$password","$db");
+$statement = $con->prepare("insert into login values(?,?)");
+$statement->bind_param("ss",$_POST['email'],$_POST['password']);
+if($statement->execute()){
+    echo "success";
+}
+else{
+    echo "Something Wrong";
+}
 ?>
