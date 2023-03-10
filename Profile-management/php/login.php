e8581fde7f82936a11b5a629621826fd13955114
Selva-NSK <99686682+Selva-NSK@users.noreply.github.com>
2023-03-10 19:41:11 +0530

    Update login.php

--- Profile-management/php/login.php
+++ Profile-management/php/login.php
@@ -2,6 +2,19 @@
 $server="localhost";
 $user="root";
 $password="root@123";
-$db="miniproject";
+$db="profile_management";
 $con= mysqli_connect("$server","$user","$password","$db");
+$statement = $con->prepare("select * from login where uname =?");
+$statement->bind_param("s",$_POST['email']);
+$statement->execute();
+$res=$statement->get_result();
+if ($row = $res->fetch_assoc()) {
+    if (($_POST['password']!= $row['password'])) {
+        echo('pwd');
+        exit;
+    }
+    echo ('verified');
+    exit;
+}
+echo 'register';
 ?>
