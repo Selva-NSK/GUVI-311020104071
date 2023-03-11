a12335f1de15e52e8e70a6741defae13508e68b0
Selva-NSK <99686682+Selva-NSK@users.noreply.github.com>
2023-03-11 20:02:37 +0530

    Update profile.js

--- Profile-management/js/profile.js
+++ Profile-management/js/profile.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
+    $("#btn").click(()=>{
+        localStorage.removeItem('info');
+        window.location='index.html';
+    })
     const item=localStorage.getItem('info');
-    const id=JSON.parse(item).id;
-    console.log(id);
-    $("p").text(id);
+    if(!item) window.location='login.html';
+    const now = new Date();
+    const t=JSON.parse(item);
+    if (now.getTime() > t.expiry){
+        localStorage.removeItem('info');
+        window.location='login.html';
+    }
+    console.log(t.id);
+    $.ajax({
+        url:'php/profile.php',
+        type: 'GET',
+        data:{email:t.id},
+        success: function(data) { 
+            const items=JSON.parse(data);
+            $('#email').text(items.email);
+            $('#name').text(items.fname+" "+items.lname);
+            $('#lname').text(items.lname);
+            $('#fname').text(items.fname);
+            $('#phone').text(items.phone);
+            $('#gender').text(items.gender);
+        },
+    });
 })
