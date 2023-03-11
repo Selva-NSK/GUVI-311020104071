6bb237fddf814c1727fd4016f50ee5fb7202ecf8
Selva-NSK <99686682+Selva-NSK@users.noreply.github.com>
2023-03-11 17:47:06 +0530

    Update register.js

--- Profile-management/js/register.js
+++ Profile-management/js/register.js
@@ -2,26 +2,57 @@ $(document).ready(function () {
     $("form").submit(function (event) {
        eml= $("#emailid").val(),
        pwd= $("#password").val(),
-      // alert(eml+" "+pwd);
-      $.ajax({
-        url:'php/register.php',
-        type: 'post',
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-         },
-        data:{email:eml,password:pwd},
-        success: function(response) { 
-          console.log(response);
-            alert("Account created");
-            const now = new Date()
-            const item = {
-              id: eml,
-              expiry: now.getTime() + 60*1000,
+       fn=$("#fname").val(),
+       ln=$("#lname").val(),
+       phn=$('#phone').val(),
+       cpwd=$('#cpass').val(),
+       output = $('input[name=gender]:checked',
+                '#frm').val();
+      if(phn.length!=10){
+        $("#phn").text("Enter 10 digit number");
+      }
+      else if(pwd.length<6){
+        $("#pwd1").text("Password must have minimum length of 6");
+      }
+      else if(pwd!=cpwd){
+        $("#pwd").text("Password doesnt match");
+      }
+      else{
+        $.ajax({
+          url:'php/register.php',
+          type: 'post',
+          headers: {
+              'Access-Control-Allow-Origin': '*'
+           },
+          data:{email:eml,password:pwd,fname:fn,lname:ln,phone:phn,gender:output},
+          success: function(response) { 
+            console.log(response);
+            if(response=='success'){
+              alert("Account created");
+              const now = new Date()
+              const item = {
+                id: eml,
+                expiry: now.getTime() + 60*1000,
+              }
+              localStorage.setItem('info', JSON.stringify(item))
+              window.location='profile.html'
             }
-            localStorage.setItem('info', JSON.stringify(item))
-            window.location='profile.html'
-        }
-      });
+            else if(response=='exist'){
+              alert('id already exists try logging in..');
+              sessionStorage.removeItem(eml);
+              window.location='login.html'
+            }
+            else{
+              alert('An '+response +' error occured');
+            } 
+          
+          }
+        });
+      }
       event.preventDefault();
     });
+  });
+  $('frm').on('click', function() {
+    console.log("l");
+    alert($('input[name=gender]:checked', '#myForm').val());
   });
