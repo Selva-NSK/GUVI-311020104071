0ed667eb62c7d16bf726eb6076c0e5c33755a855
Selva-NSK <99686682+Selva-NSK@users.noreply.github.com>
2023-03-11 20:01:01 +0530

    Update register.php

--- Profile-management/php/register.php
+++ Profile-management/php/register.php
@@ -1,34 +1,40 @@
 <?php
 require '..\vendor\autoload.php'; 
 use MongoDB\Operation\InsertOne;
-$server="localhost";
-$user="root";
-$password="root@123";
-$db="profile_management";
-$con= mysqli_connect("$server","$user","$password","$db");
-$statement = $con->prepare("select * from login where uname =?");
-$statement->bind_param("s",$_POST['email']);
-$statement->execute();
-$res=$statement->get_result();
-if ($row = $res->fetch_assoc()) {
-    echo ('exist');
-    exit;
-}
-$statement = $con->prepare("insert into login values(?,?)");
-$statement->bind_param("ss",$_POST['email'],$_POST['password']);
-if($statement->execute()){
-    try{
-        $mongo = new MongoDB\Client("mongodb://localhost:27017");
-        $db=$mongo->database->info;
-        $db->insertOne(['email'=>$_POST['email'],'fname'=>$_POST['fname'],'lname'=>$_POST['lname'],
-                      'phone'=>$_POST['phone'],'gender'=>$_POST['gender']]);
-        echo "success";
+if($_POST['infile']=='true'){
+    $server="localhost";
+    $user="root";
+    $password="root@123";
+    $db="profile_management";
+    $con= mysqli_connect("$server","$user","$password","$db");
+    $statement = $con->prepare("select * from login where uname =?");
+    $statement->bind_param("s",$_POST['email']);
+    $statement->execute();
+    $res=$statement->get_result();
+    if ($row = $res->fetch_assoc()) {
+        echo ('exist');
+        exit;
     }
-    catch(Throwable $e){
-        echo 'mongodb';
+    $statement = $con->prepare("insert into login values(?,?)");
+    $statement->bind_param("ss",$_POST['email'],$_POST['password']);
+    if($statement->execute()){
+        try{
+            $mongo = new MongoDB\Client("mongodb://localhost:27017");
+            $db=$mongo->database->info;
+            $db->insertOne(['email'=>$_POST['email'],'fname'=>$_POST['fname'],'lname'=>$_POST['lname'],
+                        'phone'=>$_POST['phone'],'gender'=>$_POST['gender']]);
+            echo "success";
+        }
+        catch(Throwable $e){
+            echo 'mongodb';
+        }
+   }
+    else{
+        echo "mysql";
     }
 }
 else{
-    echo "mysql";
+    //this section is for updating the profile
 }
+
 ?>
